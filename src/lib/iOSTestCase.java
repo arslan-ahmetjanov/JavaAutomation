4893ce63008f3bf72c195339766ca8abfda504fb
arslan <arslan.ahmetjanov@mail.ru>
2023-05-11 18:16:36 +0300

    update and fix

--- src/lib/iOSTestCase.java
+++ src/lib/iOSTestCase.java
@@ -2,6 +2,7 @@ package lib;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -9,7 +10,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.URL;
 import java.time.Duration;
 
-public class CoreTestCase extends TestCase {
+public class iOSTestCase extends TestCase {
     protected AppiumDriver driver;
     private static String AppiumURL = "http://127.0.0.1:4723";
 
@@ -20,18 +21,16 @@ public class CoreTestCase extends TestCase {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("deviceName","AndroidTestDevice");
-        capabilities.setCapability("platformVersion","8.0");
-        capabilities.setCapability("automationName","Appium");
-        capabilities.setCapability("appPackage","org.wikipedia");
-        capabilities.setCapability("appActivity",".main.MainActivity");
+        capabilities.setCapability("platformName","iOS");
+        capabilities.setCapability("deviceName","iPhone SE (3rd generation)");
+        capabilities.setCapability("platformVersion","16.4");
+        capabilities.setCapability("automationName","XCUITest");
         capabilities.setCapability("app",
-                "/Users/arslan/Desktop/course/apks/org.wikipedia.apk");
+                "/Users/arslan/Desktop/course/apps/Wikipedia.app");
         capabilities.setCapability("automationName",
                 "UiAutomator2");
 
-        driver = new AndroidDriver(new URL(AppiumURL), capabilities);
+        driver = new IOSDriver(new URL(AppiumURL), capabilities);
         this.rotateScreenPortrait();
     }
 
